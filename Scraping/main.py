a1c096d29f4062879c47a6852de5ab0b1f44b0be
alexrods <alexrodsanch@gmail.com>
2022-05-27 00:54:55 -0500

    Scraper runing 4.0

--- Scraping/main.py
+++ Scraping/main.py
@@ -8,7 +8,7 @@ from utils.scraper import scraping_categories
 def create_dataset(products: list, path: str) -> dict:
     try:
         dict_reviews = []
-        idxs = [random.randrange(0, len(products)) for _ in range(200)]
+        idxs = [random.randrange(0, len(products)) for _ in range(110)]
         for idx in idxs:
             reviews = scraping_reviews(products[idx])
             # print(reviews)
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     # category_url = "https://lista.mercadolivre.com.br/casa-moveis-decoracao/_Deal_casaedecoracao-decor#deal_print_id=e97f30c0-d00a-11ec-88f8-6dbc6c3a0c56&c_id=special-normal&c_element_order=6&c_campaign=LABEL&c_uid=e97f30c0-d00a-11ec-88f8-6dbc6c3a0c56"
     # products = scraping_categories(category_url)
-    # print(create_dataset(products, "./reviews/beleza-saude.json"))
+    # print(create_dataset(products, "./reviews/beleza_saude.json"))
 
     with open('categories/brazil.json', 'r') as f:
         categories = json.load(f)
